e12af33ef7528bbec79858c7cc8ff862e8d0fc27
Alex Bishop <alex.bishop@myob.com>
2019-11-14 15:20:00 +1100

    tidyup

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import Core.Mastermind;
-import Core.TraditionalMastermind;
-import IO.KeyPegsGeneration;
-import IO.KeyPegsGenerator;
-import IO.UserInputService;
+import Core.*;
+import IO.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        UserInputService userInputService = new UserInputService();
-        KeyPegsGeneration resultsFromUserPegs = new KeyPegsGenerator();
-        Mastermind traditionalMastermind = new TraditionalMastermind(80,resultsFromUserPegs,userInputService);
+        UserInput userInputService = new UserInputService();
+        KeyPegsGeneration keyPegsGenerator = new KeyPegsGenerator();
+        Mastermind traditionalMastermind = new TraditionalMastermind(80,keyPegsGenerator,userInputService);
         traditionalMastermind.start();
     }
 }
